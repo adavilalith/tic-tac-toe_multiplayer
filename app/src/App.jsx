ca564fc87f2e970cc8f700fc001f83c8856ce90b
Adavi Lalith <132076255+adavilalith@users.noreply.github.com>
2024-06-14 12:19:37 +0000

    adding full functionality of tic tac toe game to react app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import TicTacBoard from './components/TickTacToeBoard/TicTacBoard'
 
 function App() {
 
@@ -8,17 +9,8 @@ function App() {
       <div className="Title">
         <h1>Tic Tac Toe</h1>
       </div>
-      <div className="board">
-        <div className="cell" id="1">x</div>
-        <div className="cell" id="2">x</div>
-        <div className="cell" id="3">x</div>
-        <div className="cell" id="4">x</div>
-        <div className="cell" id="5">x</div>
-        <div className="cell" id="6">x</div>
-        <div className="cell" id="7">x</div>
-        <div className="cell" id="8">x</div>
-        <div className="cell" id="9">x</div>
-      </div>
+      <TicTacBoard/>
+
     </>
   )
 }
---
+++ app/src/components/TickTacToeBoard/TicTacBoard.jsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react'
+import "./TicTacBoard.css"
+
+export default function TicTacBoard() {
+    const [board,setBoard] = useState([" "," "," ",
+                                       " "," "," ",
+                                       " "," "," ",
+                                      ]);
+    const [turn,setTurn] = useState(0);
+    const [outputMsg,setOutputMsg] = useState("");
+    const [running,setRunning] = useState(true);
+
+    const updateBoard = (cell)=>{
+        let tempBoard = [...board]
+        if(!running){
+            setRunning(true);
+            setBoard([" "," "," ",
+                " "," "," ",
+                " "," "," ",
+               ]);
+            setOutputMsg("")
+            return;
+        }
+        console.log(cell)
+        
+        if(tempBoard[cell]!=" "){
+            setOutputMsg("Incorrect move try again!");
+            return;
+        }
+        if(turn%2){
+            tempBoard[cell] = "X";
+        }
+        else{
+            tempBoard[cell] = "O";
+        }
+        setTurn(turn+1)
+        setBoard(tempBoard);
+        setOutputMsg("")
+        checkWinner(tempBoard)
+
+    }
+
+    const checkWinner = (tempBoard)=>{
+        console.log("checking")
+        const winCond = [[0,1,2],
+                         [3,4,5],
+                         [6,7,8],
+                         [0,3,6],
+                         [1,4,7],
+                         [2,5,8],
+                         [0,4,8],
+                         [2,4,6],
+                        ]
+        for(let cond=0;cond<8;cond++){
+            console.log(winCond[cond])
+            let Xwin=true;
+            let Owin=true;
+            for(let i=0;i<3;i++){
+                if(tempBoard[winCond[cond][i]]!="X"){
+                    Xwin=false;
+                    break;
+                }
+            }
+            for(let i=0;i<3;i++){
+                if(tempBoard[winCond[cond][i]]!="O"){
+                    Owin=false;
+                    break;
+                }
+            }
+            if(Xwin){
+                setRunning(false);
+                setOutputMsg("X is the winner");
+                return;
+            }
+            if(Owin){
+                setOutputMsg("O is the winner");
+                setRunning(false);
+                return;    
+            }
+            let fullCond=true
+            for(let i=0;i<9;i++){
+                if(tempBoard[i]==" "){
+                    fullCond=false;
+                    break;
+                }
+            }
+            if(fullCond){
+                setOutputMsg("Tie");
+                setRunning(false);
+            }
+            
+        }
+    }
+
+    return (
+        <>
+        <div className="main" >
+        <div className="board">
+            <div className="cell" id="1" onClick={()=>updateBoard(0)}>{board[0]}</div>
+            <div className="cell" id="2" onClick={()=>updateBoard(1)}>{board[1]}</div>
+            <div className="cell" id="3" onClick={()=>updateBoard(2)}>{board[2]}</div>
+            <div className="cell" id="4" onClick={()=>updateBoard(3)}>{board[3]}</div>
+            <div className="cell" id="5" onClick={()=>updateBoard(4)}>{board[4]}</div>
+            <div className="cell" id="6" onClick={()=>updateBoard(5)}>{board[5]}</div>
+            <div className="cell" id="7" onClick={()=>updateBoard(6)}>{board[6]}</div>
+            <div className="cell" id="8" onClick={()=>updateBoard(7)}>{board[7]}</div>
+            <div className="cell" id="9" onClick={()=>updateBoard(8)}>{board[8]}</div>
+        </div>
+        </div>
+        <div id="outputMsg">
+            <h1>
+        {outputMsg}
+
+            </h1>
+        </div>
+        
+        </>
+    )
+}
