7a8c357a88ab958b2e086b7ae270af8e5e1b9d7f
Adavi Lalith <132076255+adavilalith@users.noreply.github.com>
2024-08-10 10:22:07 +0000

    adding vercel backend link

--- app/src/config/backendURL.js
+++ app/src/config/backendURL.js
@@ -1,2 +1,2 @@
 
-export const backendURL = "https://tictactoebackendglitch.glitch.me"//"https://psychic-doodle-vxr44vj9jj4fp4r4-8080.app.github.dev"
+export const backendURL = "https://tic-tac-toe-backend-iota.vercel.app/"//"https://tictactoebackendglitch.glitch.me"//"https://psychic-doodle-vxr44vj9jj4fp4r4-8080.app.github.dev"
