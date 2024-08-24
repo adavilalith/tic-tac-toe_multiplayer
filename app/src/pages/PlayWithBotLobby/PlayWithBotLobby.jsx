0258af22adb07c0988b8350463a8ad318f4754de
Lalith Adavi <adavilalith11@gmail.com>
2024-08-24 15:32:04 -0700

    finished front end for bot lobby

--- app/src/pages/PlayWithBotLobby/PlayWithBotLobby.jsx
+++ app/src/pages/PlayWithBotLobby/PlayWithBotLobby.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './PlayWithBotLobby.css'
 
 
 export default function PlayWithBotLobby() {
+  const [difficultyLevel,setDifficultyLevel] = useState(2)
+  let colors = ["greenyellow","green","yellow","orange","red"]
+  const difficulties = ["Impossible To Lose", "Easy Peasy", "I want to Play","Give me a Challenge ","Impossible To Win"]
   return (
     <div id="main">
-      <div>
-      <h1 className="Title">Player Vs Bot</h1>
-      </div>      
-      <input type="range" max="5" min="1"
-       />
+      <div id="Title">
+        <h1 >Player</h1>
+        <h1>vs</h1>
+        <h1>Bot</h1>
+      </div>
+      <h2>Select Diffuculty:</h2>     
+      <h2 style={{"color": `${colors[difficultyLevel-1]}`}}>{`${difficulties[difficultyLevel-1]}`}</h2> 
+      <input id="difficulty-slider" type="range" max="5" min="1" value={difficultyLevel} onChange={(e)=>setDifficultyLevel(e.target.value)} style={{accentColor: `${colors[difficultyLevel-1]}`}}/>
+      <button id="start-game-btn">Start Game</button>
     </div>
   )
 }
